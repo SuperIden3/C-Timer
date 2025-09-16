2101d7da3bbc11a5b74277cfcc1d411a7b37468c
Chi Zu <chizu7993@gmail.com>
2025-09-16 18:40:40 -0400

    Started working on `SIGTSTP` handle.
    Instead of stopping the program, it will continue running in the
    background, just not printing anything to `stdout`.

--- src/timer.c
+++ src/timer.c
@@ -4,9 +4,26 @@
 #include <stdio.h> // printf
 #include <unistd.h> // sleep
 #include <stdlib.h> // strtoull
+#include <signal.h> // Handle SIGTSTP (Ctrl+Z)
+#include <stdbool.h> // Manage SIGTSTP using a `bool`
+
+volatile bool sigtstp = false;
+
+void sigtstp_handler(int code) {
+  sigtstp = code == SIGTSTP;
+}
 
 int main(int argc, char **argv) {
-  const int ISATTY = isatty(STDOUT_FILENO); // Check if stdout is a tty for those who want to play with this source coude in a plain-text output.
+  const bool ISATTY = isatty(STDOUT_FILENO) == 1; // Check if stdout is a tty for those who want to play with this source coude in a plain-text output.
+
+  struct sigaction sa;             // For sigaction()
+  sa.sa_handler = sigtstp_handler; // Set handler
+  sigemptyset(&sa.sa_mask);        // Clear signal masks
+  sa.sa_flags = 0;                 // No special flags
+  if (sigaction(SIGTSTP, &sa, NULL) == -1) {
+    perror("sigaction() failed, cannot set handler for SIGTSTP");
+  }
+
   timer_t seconds       = 0;
   timer_t minutes       = 0;
   timer_t hours         = 0;
@@ -44,16 +61,20 @@ int main(int argc, char **argv) {
   total_seconds = as_seconds(hours, minutes, seconds);
   while (total_seconds > 0) {
     // Format & print time and decrement total seconds.
-    printf("%02llu:%02llu:%02llu", hours, minutes, seconds);
-    if (ISATTY) {
-      printf("\r");
-      fflush(stdout);
-    } else printf("\n");
+    if (!sigtstp) {
+      printf("%02llu:%02llu:%02llu", hours, minutes, seconds);
+      if (ISATTY) {
+        printf("\r");
+        fflush(stdout);
+      } else {
+        printf("\n");
+      }
+    }
     sleep(1);
     total_seconds--;
 
     // Decrement time if needed.
-    if (seconds == 0)
+    if (seconds == 0) {
       if (minutes == 0) { // If there are 0 minutes on timer, reset to 59 minutes and borrow from hours.
         hours--;
         minutes = 59;
@@ -62,10 +83,12 @@ int main(int argc, char **argv) {
         minutes--;
         seconds = 59;
       }
-    else seconds--;
+    } else {
+      seconds--;
+    }
   }
 
-  printf("00:00:00");
+  if (!sigtstp) printf("00:00:00");
   if (ISATTY) printf("\007");
   printf("\n");
   return 0;
